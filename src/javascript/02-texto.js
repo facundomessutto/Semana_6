665af50525d26b70f70419c8a2b69e4a8af4a105
facundomessutto <facundo.messutto@gmail.com>
2026-04-20 23:22:26 -0400

    Create 02-texto.js y 05-alert.js

---
+++ src/javascript/02-texto.js
@@ -0,0 +1,31 @@
+
+//Definimos variables con "var"
+var nombre = "Facundo";
+var apellido = "Messutto";
+var edad = 20;
+
+//Mostrar texto en console.log
+console.log(nombre);
+console.log(apellido);
+console.log(nombre + " " + apellido +" " +edad );
+console.log(nombre, apellido, edad);
+
+
+var nom_ape = nombre+" "+apellido;
+console.log(nom_ape," "+edad);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/javascript/05-alert.js
@@ -0,0 +1,27 @@
+
+
+//Definir variables
+var pais="Chile";
+var continente="Sur America";
+
+//alert(pais);
+//alert(continente);
+
+let a =12;
+let b=73;
+
+alert(a+b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
