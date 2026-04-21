b989409ecf4e27e40a4d8a2fc3b5bd1636d4bfeb
facundomessutto <facundo.messutto@gmail.com>
2026-04-20 22:56:10 -0400

    Create a index, style and js
    Se creo archivos index.html (Pagina Web), style.css (Estilos de pagina web )y 01-matematicas.js (Ejercicios matematicos)

---
+++ src/javascript/01-matematicas.js
@@ -0,0 +1,79 @@
+
+//define variables
+let x = 2;
+let y = 4;
+
+//mostrar el valor de las variables
+console.log('La variable "X" tiene el valor: ',x);
+console.log(y);
+
+console.log("----------------------");
+
+//Operaciones matematicas
+console.log('Suma: ',x+y);
+console.log('Resta: ',x-y);
+console.log('Multiplicación: ',x*y);
+console.log('División: ',x/y);
+
+//Transformación de un int a un string
+console.log('Esto es un Parse String: '+(x/y));
+
+//Definimos Booleanos
+let t= true;
+let f= false;
+
+//Comparativos de valores Booleanos
+console.log("Si comparamos t con t nos da: ",t == t);
+console.log("Si comparamos t con f nos da: ",t == f);
+console.log("Si comparamos f con f nos da: ",f == f);
+
+console.log("-------------------------");
+
+//Comparativa una o otra
+console.log("Si es t o t nos da: ",t || t);
+console.log("Si es t o f nos da: ",t || f);
+console.log("Si es f o f nos da: ",f || f);
+console.log("Si es f o t nos da: ",f || t);
+
+console.log("-------------------------");
+
+//Cumplir ambas condiciones
+console.log("Si elegimos entre t y t tenemos: ",t && t);
+console.log("Si elegimos entre f y f tenemos: ",f && f);
+console.log("Si elegimos entre t y f tenemos: ",t && f);
+console.log("Si elegimos entre f y t tenemos: ",f && t);
+
+console.log("-------------------------");
+
+//Distinto
+console.log("El t es distinto a f: ",t != f);
+console.log("El t es distinto a t: ",t != t);
+console.log("El f es distinto a f: ",f != f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
